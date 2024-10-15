8a498af3d581c0da37ab46a990659374b2e19de9
Nikita Lopanov <n.lopanov@allfuneral.com>
2024-10-15 23:26:19 +0300

    tailwind configs changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+      <body className={cn('min-h-screen font-sans antialiased', fontSans.variable)}>
         <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
